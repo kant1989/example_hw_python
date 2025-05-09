db719c6bd47b0401aa68c5266b79b4948961288e
Kantemir Koshiev <kkoshiev@gmail.com>
2025-05-09 13:32:08 +0300

    добавление декораторов для allure отчетов

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,5 +1,6 @@
 import os
 
+import allure
 from selene import browser, have, command
 import resources
 from data.users import User
@@ -11,6 +12,7 @@ class RegistrationPage:
         self.google_ads = "[id^=google_ads][id$=container__]"
         self.select_option = "[id^=react-select][id*=option]"
 
+    @allure.step("Открытие страницы авторизации")
     def open(self):
         browser.open("/automation-practice-form")
         browser.all(self.google_ads).with_(timeout=10).wait_until(
@@ -19,26 +21,32 @@ class RegistrationPage:
         browser.all(self.google_ads).perform(command.js.remove)
         return self
 
+    @allure.step("Заполнение имени")
     def fill_first_name(self, value):
         browser.element("#firstName").type(value)
         return self
 
+    @allure.step("Заполнение фамилии")
     def fill_last_name(self, value):
         browser.element("#lastName").type(value)
         return self
 
+    @allure.step("Выбор пола")
     def select_gender(self, value):
         browser.all("[name=gender]").element_by(have.value(value)).element("..").click()
         return self
 
+    @allure.step("Заполнение емайл")
     def fill_email(self, value):
         browser.element("#userEmail").type(value)
         return self
 
+    @allure.step("Заполнение телефона")
     def fill_phone_number(self, value):
         browser.element("#userNumber").type(value)
         return self
 
+    @allure.step("Выбор хобби")
     def select_hobbies(self, *args):
         for hobby in args:
             browser.all("label[for^=hobbies-checkbox]").element_by(
@@ -46,20 +54,24 @@ class RegistrationPage:
             ).click()
         return self
 
+    @allure.step("Заполнение адреса")
     def fill_address(self, value):
         browser.element("#currentAddress").type(value)
         return self
 
+    @allure.step("Выбор региона")
     def select_state(self, value):
         browser.element("#state").click()
         browser.all(self.select_option).element_by(have.exact_text(value)).click()
         return self
 
+    @allure.step("Выбор города")
     def select_city(self, value):
         browser.element("#city").click()
         browser.all(self.select_option).element_by(have.exact_text(value)).click()
         return self
 
+    @allure.step("Заполнение даты рождения")
     def fill_date_of_birth(self, year, month, day):
         browser.element("#dateOfBirthInput").click()
         browser.element(".react-datepicker__month-select").type(month)
@@ -67,17 +79,20 @@ class RegistrationPage:
         browser.element(f".react-datepicker__day--0{day}").click()
         return self
 
+    @allure.step("Заолнение предметов")
     def fill_subjects(self, *args):
         for subject in args:
             browser.element("#subjectsInput").type(subject).press_enter()
         return self
 
+    @allure.step("Выбор фотографии")
     def select_picture(self, filename):
         browser.element("#uploadPicture").set_value(
             os.path.abspath(os.path.join(os.path.dirname(resources.__file__), filename))
         )
         return self
 
+    @allure.step("Нажатие кнопки подтверждения регистрации")
     def submit(self):
         browser.element("#submit").press_enter()
         return self
@@ -89,7 +104,7 @@ class RegistrationPage:
         self.select_gender(user.gender)
         self.fill_phone_number(user.phone_number)
         self.fill_date_of_birth(
-            user.date_of_birth.strftime("%Y"),
+            user.date_of_birth.strftime("2000"),
             user.date_of_birth.strftime("%B"),
             user.date_of_birth.strftime("%d"),
         )
@@ -102,6 +117,7 @@ class RegistrationPage:
         self.submit()
         return self
 
+    @allure.step("Проверка данных зарегистрированного пользователя")
     def should_have_registered(self, user: User):
         browser.element(".table").all("td").even.should(
             have.exact_texts(
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,8 +1,16 @@
+import allure
+
 from data import users
 from pages.application import app
 
 
+@allure.label("owner", "Kantemir Koshiev")
+@allure.feature("Регистрация")
+@allure.story("Регистрация студента")
 def test_student_registration_form():
+    """
+    Регистрация пользователя и проверка данных пользователя после регистрации
+    """
     app.registration_page.open()
 
     # WHEN
